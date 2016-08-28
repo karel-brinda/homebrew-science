5a643b98220ead152fd38b04ea626531c13803a5
Karel Břinda <karel.brinda@gmail.com>
2016-08-28 16:15:13 +0200

    Fix minor problems

--- ococo.rb
+++ ococo.rb
@@ -1,7 +1,7 @@
 class Ococo < Formula
   desc "Ococo, the first online consensus caller"
   homepage "https://github.com/karel-brinda/ococo"
-  url "https://github.com/karel-brinda/ococo/archive/0.1.2.1.tar.gz"
+  url "https://github.com/karel-brinda/ococo/archive/#{version}.tar.gz"
   version "0.1.2.1"
   sha256 "9fb55b1fad3647bc651a638851fe39c5c7151a0718034f22bf3129e29a1d965e"
 
@@ -10,7 +10,7 @@ class Ococo < Formula
   depends_on "htslib"
 
   def install
-    system "make HTSLIBINCLUDE=#{Formula["htslib"].opt_prefix}/include HTSLIB=#{Formula["htslib"].opt_prefix}/lib/libhts.a"
+    system ["make", "HTSLIBINCLUDE=#{Formula["htslib"].opt_prefix}/include", "HTSLIB=#{Formula["htslib"].opt_prefix}/lib/libhts.a"]
     bin.install "ococo"
     man1.install "ococo.1"
   end
