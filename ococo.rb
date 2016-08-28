166c380c22ce4ab08729cccb6c08e05c1b81c195
Karel Břinda <karel.brinda@gmail.com>
2016-08-28 17:27:28 +0200

    Define version before url

--- ococo.rb
+++ ococo.rb
@@ -1,8 +1,8 @@
 class Ococo < Formula
   desc "Ococo, the first online consensus caller"
   homepage "https://github.com/karel-brinda/ococo"
-  url "https://github.com/karel-brinda/ococo/archive/#{version}.tar.gz"
   version "0.1.2.1"
+  url "https://github.com/karel-brinda/ococo/archive/#{version}.tar.gz"
   sha256 "9fb55b1fad3647bc651a638851fe39c5c7151a0718034f22bf3129e29a1d965e"
 
   head "https://github.com/karel-brinda/ococo.git"
