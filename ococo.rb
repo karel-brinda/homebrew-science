dab78d1e457a06f25275ca895553ea1d8ca939a4
Karel Břinda <karel.brinda@gmail.com>
2016-08-28 16:03:17 +0200

    Add Ococo

---
+++ ococo.rb
@@ -0,0 +1,21 @@
+class Ococo < Formula
+  desc "Ococo, the first online consensus caller"
+  homepage "https://github.com/karel-brinda/ococo"
+  url "https://github.com/karel-brinda/ococo/archive/0.1.2.1.tar.gz"
+  version "0.1.2.1"
+  sha256 "9fb55b1fad3647bc651a638851fe39c5c7151a0718034f22bf3129e29a1d965e"
+
+  head "https://github.com/karel-brinda/ococo.git"
+
+  depends_on "htslib"
+
+  def install
+    system "make HTSLIBINCLUDE=#{Formula["htslib"].opt_prefix}/include HTSLIB=#{Formula["htslib"].opt_prefix}/lib/libhts.a"
+    bin.install "ococo"
+    man1.install "ococo.1"
+  end
+
+  test do
+    system"#{bin}/ococo -v"
+  end
+end
